bc345ca2d55f057cb2fb2d9c489ad35a22bcdf9e
Lucas Oliveira <lucasnoliveira126@gmail.com>
2022-09-27 17:45:46 -0300

    snake created

--- snake.js
+++ snake.js
@@ -5,6 +5,13 @@ export function update(){
     console.log('update snake')
 }
 
-export function draw(){
+export function draw(gameBoard){
     console.log('draw snake')
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
 }
