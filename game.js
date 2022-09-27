2c7aa5047cee56d35057c239d00d6e4f52a3638c
Lucas Oliveira <lucasnoliveira126@gmail.com>
2022-09-27 17:30:31 -0300

    snake body

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
+
 let lastRenderTime = 0
-const  SNAKE_SPEED = 2
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
@@ -7,6 +8,17 @@ function main(currentTime) {
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
     console.log ('Render')
     lastRenderTime = currentTime
+
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
+
+function update () {
+    updateSnake()
+}
+
+function draw() {
+    drawSnake()
+}
